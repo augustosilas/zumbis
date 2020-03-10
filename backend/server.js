fbe21abb685ca72278029243978664bdda973fc8
Silas Augusto <augusto.fernandes775@gmail.com>
2020-03-10 17:59:43 -0400

    conexão com banco de dados mysql
    Signed-off-by: Silas Augusto <augusto.fernandes775@gmail.com>

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,31 @@
 const express = require('express');
+const mysql = require('mysql');
 
+// Iniciando o app
 const app = express();
 
-app.listen(3001);
+// Iniciando o banco de dados
+
+const connection = mysql.createConnection({
+    host: "127.0.0.1",
+    port: "3306",
+    user: "root",
+    password: "root",
+    database: "BancoZumbi"
+});
+
+connection.connect((err) => {
+    if(!err) {
+        console.log('Connected!');
+    } else {
+        console.log('Connected Falied!');
+    }
+
+})
+
+// Primeira rota
+app.get('/', (req, res) => {
+    res.send('Olá Mundo')
+});
+
+app.listen(3003);
