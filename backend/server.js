977e9fb6c599a6f6d983b3bd88e2c9e47a057bd8
Silas Augusto <augusto.fernandes775@gmail.com>
2020-03-11 00:43:09 -0400

    conectando com o BD mongo
    Signed-off-by: Silas Augusto <augusto.fernandes775@gmail.com>

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 // Iniciando o app
 const app = express();
 
-// Iniciando o banco de dados
-
+// Iniciando o BD
+// É necessario passar a url de conexão com o mongodb
+// nodeapi: nome do schema a ser utilizado
+mongoose.connect('mongodb://localhost:27017/nodeapi', 
+{ useUnifiedTopology: true,
+    useNewUrlParser: true });
 
 // Primeira rota
 app.get('/', (req, res) => {
