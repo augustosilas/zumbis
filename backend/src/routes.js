0ec4ea8d61cc2342cef71ebcb01e51443bc05b66
Silas Augusto <augusto.fernandes775@gmail.com>
2020-03-11 02:44:05 -0400

    adição de novas rotas
    Signed-off-by: Silas Augusto <augusto.fernandes775@gmail.com>

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,8 +5,11 @@ const ArmaController = require('./controllers/ArmaController');
 const ArmaduraController = require('./controllers/ArmaduraController');
 const ZumbiController = require('./controllers/ZumbiController')
 
-routes.get('/armas', ArmaController.index);
-routes.get('/armaduras', ArmaduraController.index);
-routes.get('/zumbi', ZumbiController.index);
+routes.get('/armas', ArmaController.index);             // exibe todos elementos
+routes.post('/armas', ArmaController.store);            // cria novos elementos
+
+routes.get('/armaduras', ArmaduraController.index);     // exibe todos elementos
+
+routes.get('/zumbi', ZumbiController.index);            // exibe todos elementos
 
 module.exports = routes;
