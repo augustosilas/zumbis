3bf249d63bb7e781f99cc0046a23a77cf57d97a3
Silas Augusto <augusto.fernandes775@gmail.com>
2020-03-10 23:20:33 -0400

    recriando conexão com banco de dados
    Signed-off-by: Silas Augusto <augusto.fernandes775@gmail.com>

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,10 @@
 const express = require('express');
-const mysql = require('mysql');
 
 // Iniciando o app
 const app = express();
 
 // Iniciando o banco de dados
 
-const connection = mysql.createConnection({
-    host: "127.0.0.1",
-    port: "3306",
-    user: "root",
-    password: "root",
-    database: "BancoZumbi"
-});
-
-connection.connect((err) => {
-    if(!err) {
-        console.log('Connected!');
-    } else {
-        console.log('Connected Falied!');
-    }
-
-})
 
 // Primeira rota
 app.get('/', (req, res) => {
