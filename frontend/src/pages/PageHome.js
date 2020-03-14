c83163031ce6380d647b4eed2cf186c52bd70989
Silas Augusto <augusto.fernandes775@gmail.com>
2020-03-14 04:06:07 -0400

    telas do app
    Signed-off-by: Silas Augusto <augusto.fernandes775@gmail.com>

---
+++ frontend/src/pages/PageHome.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import { View, Button, Text } from 'react-native';
+
+export default class PageHome extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Primeira Página</Text>
+      </View>
+    );
+  }
+}
