d7eda488b3b4805c4f33489c438ac7f3b33405fb
Cristian Peres <christyansoftperes@gmail.com>
2020-03-17 15:33:33 -0300

    Adicionando separador
    Signed-off-by: Cristian Peres <christyansoftperes@gmail.com>

--- frontend/src/pages/PageHome.js
+++ frontend/src/pages/PageHome.js
@@ -1,19 +1,20 @@
 import * as React from 'react';
-import {Button, View, Text} from 'react-native';
+import {Button, View, Text, StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
 function HomeScreen({navigation}) {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home</Text>
       <Button title="Arma" onPress={() => navigation.navigate('Arma')} />
+      <Separator />
       <Button
         title="Armadura"
         onPress={() => {
           navigation.navigate('Armadura');
         }}
       />
+      <Separator />
       <Button
         title="Zumbi"
         onPress={() => {
@@ -23,5 +24,17 @@ function HomeScreen({navigation}) {
     </View>
   );
 }
+
+function Separator() {
+  return <View style={styles.separator} />;
+}
+
+const styles = StyleSheet.create({
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+});
 
 export default HomeScreen;
