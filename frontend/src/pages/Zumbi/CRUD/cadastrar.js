ebfa9713d30721bef0b1faa5b033ff135b7bf0b2
Cristian Peres <christyansoftperes@gmail.com>
2020-03-17 18:20:18 -0300

    crud zumbi
    Signed-off-by: Cristian Peres <christyansoftperes@gmail.com>

---
+++ frontend/src/pages/Zumbi/CRUD/cadastrar.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {
+  View,
+  Button,
+  Text,
+  TextInput,
+  StyleSheet,
+  CheckBox,
+} from 'react-native';
+
+const cadastrar = ({navigation}) => {
+  return <View />;
+};
