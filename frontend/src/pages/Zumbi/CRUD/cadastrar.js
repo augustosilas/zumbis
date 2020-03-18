118dc8a3389abde04a14e9df21fb37ddc82bc44f
Cristian Peres <christyansoftperes@gmail.com>
2020-03-18 18:26:48 -0300

    implementação parcial do cadastro de zumbi

--- frontend/src/pages/Zumbi/CRUD/cadastrar.js
+++ frontend/src/pages/Zumbi/CRUD/cadastrar.js
@@ -5,9 +5,61 @@ import {
   Text,
   TextInput,
   StyleSheet,
-  CheckBox,
+  SafeAreaView,
 } from 'react-native';
 
 const cadastrar = ({navigation}) => {
-  return <View />;
+  let [nomeZ, setNome] = React.useState();
+  let [armas, setArmas] = React.useState();
+  let [armaduras, setArmaduras] = React.useState();
+
+  return (
+    <View>
+      <Text>Cadastrar Zumbi</Text>
+      <View>
+        <Text>Nome</Text>
+        <TextInput
+          onChangeText={text => setNome(text)}
+          value={nomeZ}
+          placeholder="Nome"
+        />
+        <Text>Armas</Text>
+        <TextInput
+          onChangeText={text => setArmas(text)}
+          value={armas}
+          placeholder="Armas (mais de uma separadas por virgula)"
+        />
+        <Text>Armaduras</Text>
+        <TextInput
+          onChangeText={text => setArmaduras(text)}
+          value={armaduras}
+          placeholder="Armaduras (mais de uma separadas por virgula)"
+        />
+        <Button
+          title="Cadastrar"
+          onPress={() => {
+            let armasSplit = '';
+            let armadurasSplit = '';
+            if (armas !== undefined) {
+              armasSplit = armas.split(',');
+            }
+            if (armaduras !== undefined) {
+              armadurasSplit = armaduras.split(',');
+            }
+
+            const zumbi = {
+              nome: nomeZ,
+              armas: armasSplit,
+              armaduras: armadurasSplit,
+            };
+
+            const zumbiJSON = JSON.stringify(zumbi);
+            console.log(zumbiJSON);
+          }}
+        />
+      </View>
+    </View>
+  );
 };
+
+export default cadastrar;
