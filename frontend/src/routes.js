23eb961661364f0bcb263ec7e74a631d15678466
Cristian Peres <christyansoftperes@gmail.com>
2020-03-18 15:26:33 -0300

    arquivo com todas rotas
    Signed-off-by: Cristian Peres <christyansoftperes@gmail.com>

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,10 +1,38 @@
-import PageHome from './pages/PageHome'
+import PageHome from './pages/PageHome';
+import PageZumbi from './pages/Zumbi/PageZumbi';
+import CadastroZumbi from './pages/Zumbi/CRUD/cadastrar';
+import BuscaZumbi from './pages/Zumbi/CRUD/buscar';
+import EditaZumbi from './pages/Zumbi/CRUD/editar';
 
-import { createStackNavigator } from 'react-navigation-stack';
-import { createAppContainer } from 'react-navigation'
+import PageArma from './pages/Arma/PageArma';
+import CadastroArma from './pages/Arma/CRUD/cadastrar';
+import EditarArma from './pages/Arma/CRUD/editar';
+import BuscaArma from './pages/Arma/CRUD/buscar';
 
-export default createAppContainer(
-    createStackNavigator({
-        PageHome,
-    })
+import PageArmadura from './pages/Armadura/PageArmadura';
+import CadastroArmadura from './pages/Armadura/CRUD/cadastrar';
+import BuscaArmadura from './pages/Armadura/CRUD/buscar';
+
+import {createStackNavigator} from 'react-navigation-stack';
+import {createAppContainer} from 'react-navigation';
+
+const Routes = createAppContainer(
+  createStackNavigator({
+    PageHome: PageHome,
+    PageZumbi: PageZumbi,
+    CadastroZumbi: CadastroZumbi,
+    BuscaZumbi: BuscaZumbi,
+    EditaZumbi: EditaZumbi,
+
+    PageArma: PageArma,
+    CadastroArma: CadastroArma,
+    EditarArma: EditarArma,
+    BuscaArma: BuscaArma,
+
+    PageArmadura: PageArmadura,
+    CadastroArmadura: CadastroArmadura,
+    BuscaArmadura: BuscaArmadura,
+  }),
 );
+
+export default Routes;
