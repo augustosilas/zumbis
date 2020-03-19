bbc665c2883badbb9a91075e1717880634239f75
Cristian Peres <christyansoftperes@gmail.com>
2020-03-19 18:14:42 -0300

    modificação na porta

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const requireDir = require('require-dir');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const requireDir = require("require-dir");
 
 // Iniciando o app
 const app = express();
@@ -13,17 +13,17 @@ app.use(cors());
 // Iniciando o BD
 // É necessario passar a url de conexão com o mongodb
 // nodeapi: nome do schema a ser utilizado
-mongoose.connect('mongodb://localhost:17017/nodeapi', 
-{ useNewUrlParser: true });
-requireDir('./src/models')
+mongoose.connect("mongodb://localhost:27017/nodeapi", {
+  useNewUrlParser: true
+});
+requireDir("./src/models");
 
-const Arma = mongoose.model('Arma');            // Acesso a Arma: inserir, editar e deletar
-const Armadura = mongoose.model('Armadura');    // Acesso a Armadura: inserir, editar e deletar
-const Zumbi = mongoose.model('Zumbi');          // Acesso a Zumbi: inserir, editar e deletar
-
+const Arma = mongoose.model("Arma"); // Acesso a Arma: inserir, editar e deletar
+const Armadura = mongoose.model("Armadura"); // Acesso a Armadura: inserir, editar e deletar
+const Zumbi = mongoose.model("Zumbi"); // Acesso a Zumbi: inserir, editar e deletar
 
 // Rotas
 // 'use' é um coringa, aceita todo tipo de requisições
-app.use('/api', require('./src/routes'))
+app.use("/api", require("./src/routes"));
 
-app.listen(3003)
+app.listen(3003);
