505c73ac55329675552a7b2c95c7cd891b651590
Silas Augusto <augusto.fernandes775@gmail.com>
2020-03-23 23:34:58 -0300

    remoção de condições desnecessarias

--- backend/src/controllers/ZumbiController.js
+++ backend/src/controllers/ZumbiController.js
@@ -1,49 +1,51 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Importando model 'Zumbi'
-const Zumbi = mongoose.model('Zumbi');
-const Arma = mongoose.model('Arma');
-const Armadura = mongoose.model('Armadura');
+const Zumbi = mongoose.model("Zumbi");
+const Arma = mongoose.model("Arma");
+const Armadura = mongoose.model("Armadura");
 
 module.exports = {
-    async index(req, res) {
-        const { page = 1 } = req.query;
-        console.log(req);
-        // Primeiro parâmetro: query, condições de filtro
-        // Segundo parâmetro: página atual e limite de elementos
-        let zumbi = await Zumbi.paginate({}, {page, limit: 10});
+  async index(req, res) {
+    const { page = 1 } = req.query;
+    console.log(req);
+    // Primeiro parâmetro: query, condições de filtro
+    // Segundo parâmetro: página atual e limite de elementos
+    let zumbi = await Zumbi.paginate({}, { page, limit: 10 });
 
-        return res.json(zumbi);
-    },
+    return res.json(zumbi);
+  },
 
-    async store(req, res) {
-        let { arma, armadura } = req.body;
+  async store(req, res) {
+    // let { arma, armadura } = req.body;
 
-        let nome = arma;
-        let zumbiArma = await Arma.findOne({ nome });
-                
-        nome = armadura;
-        let zumbiArmadura = await Armadura.findOne({ nome });
-        
-        if(zumbiArma != null && zumbiArmadura != null) {
-            const zumbi = await Zumbi.create(req.body);      
-            return res.json(res.json(zumbi));
-        }
+    // let nome = arma;
+    // let zumbiArma = await Arma.findOne({ nome });
 
-        return res.send("Impossivel criar zumbi!!!");
-    },
+    // nome = armadura;
+    // let zumbiArmadura = await Armadura.findOne({ nome });
 
-    async update(req, res) {
-        // {new: true} força o método retornar o valor atualizado para a variavel 'zumbi'
-        let zumbi = await Zumbi.findByIdAndUpdate(req.params.id, req.body, {new: true});
+    // if(zumbiArma != null && zumbiArmadura != null) {
+    console.log(req.body);
+    const zumbi = await Zumbi.create(req.body);
+    return res.json(zumbi);
+    // }
 
-        return res.json(zumbi);
-    },
+    // return res.send("Impossivel criar zumbi!!!");
+  },
 
-    async destroy(req, res) {
-        await Zumbi.findByIdAndRemove(req.params.id);
+  async update(req, res) {
+    // {new: true} força o método retornar o valor atualizado para a variavel 'zumbi'
+    let zumbi = await Zumbi.findByIdAndUpdate(req.params.id, req.body, {
+      new: true
+    });
 
-        return res.send();
-    }
-}
+    return res.json(zumbi);
+  },
 
+  async destroy(req, res) {
+    await Zumbi.findByIdAndRemove(req.params.id);
+
+    return res.send();
+  }
+};
