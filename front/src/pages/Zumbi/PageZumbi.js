72fb1fea708ec83f3d30ddb0de770ea86f235ff3
Silas Augusto <augusto.fernandes775@gmail.com>
2020-03-23 23:36:41 -0300

    remoção de informações desnecessarias e mundando de função para classe

--- front/src/pages/Zumbi/PageZumbi.js
+++ front/src/pages/Zumbi/PageZumbi.js
@@ -1,111 +1,94 @@
-import React from 'react';
-import {View, Text, StyleSheet, ScrollView} from 'react-native';
+import React, {Component} from 'react';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
 
 import {Button, List, ListItem} from '@ui-kitten/components';
 
-const DATA = [
-  [
-    [
-      {
-        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-        name: 'Bazuca',
-        calibri: 'nem sei',
-        dano: 'vish',
-      },
-      {
-        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-        name: 'Pistola',
-        calibri: '.40',
-        dano: 'eu hem',
-      },
-    ],
-    [
-      {
-        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-        nome: 'Casaco',
-        absorcao: 'já era',
-      },
-      {
-        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-        nome: 'Armadura de ferro',
-        absorcao: 'deu bom',
-      },
-    ],
-  ],
-  [
-    [
-      {
-        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-        name: 'Bazuca',
-        calibri: 'nem sei',
-        dano: 'vish',
-      },
-      {
-        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-        name: 'Pistola',
-        calibri: '.40',
-        dano: 'eu hem',
-      },
-    ],
-    [
-      {
-        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-        nome: 'Casaco',
-        absorcao: 'já era',
-      },
-      {
-        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-        nome: 'Armadura de ferro',
-        absorcao: 'deu bom',
-      },
-    ],
-  ],
-];
-
-const PageZumbi = ({navigation}) => {
-  let zumbi = DATA;
-  const onSelected = () => {
-    navigation.navigate('CadastroZumbi');
+export default class PageZumbi extends Component {
+  state = {
+    zumbi: {arma: [], armadura: []},
   };
 
-  const renderItemAccessory = (styles, index, item) => (
+  render() {
+    return (
+      <>
+        <View>
+          <Button
+            appearance={'filled'}
+            onPress={() => {
+              this.props.navigation.navigate('CadastroZumbi');
+            }}>
+            Cadastrar
+          </Button>
+        </View>
+        <FlatList
+          data={}
+          keyExtractor={key => key.id}
+          renderItem={''}></FlatList>
+      </>
+    );
+  }
+}
+
+function renderItemAccessory(styles, index, item) {
+  return (
     <View>
       <Button onPress={'remove'}>Deletar</Button>
     </View>
   );
+}
 
-  const renderZumbi = ({item, index}) => {
-    const buildStringArmas = () => {};
-    const buildStringArmaduras = () => {};
-
-    return (
-      <ListItem
-        title={`Zumbi ${index}`}
-        description={`Armas: ${item.calibri}\n Armaduras: ${item.dano}`}
-        accessory={() => renderItemAccessory(styles, index, item)}
-        onPress={onSelected}
-      />
-    );
-  };
-
+function renderZumbi({item, index}) {
   return (
-    <>
-      <View>
-        <Button
-          appearance={'filled'}
-          onPress={() => {
-            navigation.navigate('CadastroZumbi');
-          }}>
-          Cadastrar
-        </Button>
-      </View>
-      <Text>Armas</Text>
-      <ScrollView>
-        <List data={zumbi} renderItem={renderZumbi} />
-      </ScrollView>
-    </>
+    <ListItem
+      title={`Zumbi ${index}`}
+      description={`Armas: ${item.calibri}\n Armaduras: ${item.dano}`}
+      accessory={() => renderItemAccessory(styles, index, item)}
+      onPress={onSelected}
+    />
   );
-};
+}
+
+// const PageZumbi = ({navigation}) => {
+//   let zumbi = DATA;
+//   const onSelected = () => {
+//     navigation.navigate('CadastroZumbi');
+//   };
+
+// const renderItemAccessory = (styles, index, item) => (
+//   <View>
+//     <Button onPress={'remove'}>Deletar</Button>
+//   </View>
+// );
+
+// const renderZumbi = ({item, index}) => {
+//   return (
+//     <ListItem
+//       title={`Zumbi ${index}`}
+//       description={`Armas: ${item.calibri}\n Armaduras: ${item.dano}`}
+//       accessory={() => renderItemAccessory(styles, index, item)}
+//       onPress={onSelected}
+//     />
+//   );
+// };
+
+//   return (
+// <>
+//   <View>
+//     <Button
+//       appearance={'filled'}
+//       onPress={() => {
+//         navigation.navigate('CadastroZumbi');
+//       }}>
+//       Cadastrar
+//     </Button>
+//   </View>
+//   <Text>Armas</Text>
+//   <ScrollView>
+//     <List data={zumbi} renderItem={renderZumbi} />
+//   </ScrollView>
+// </>
+//   );
+// };
 
 const styles = StyleSheet.create({
   item: {
@@ -119,4 +102,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default PageZumbi;
+// export default PageZumbi;
