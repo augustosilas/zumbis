3a0bccb47754df9a7bbc04ce6dcc77a090f8e1d9
Silas Augusto <augusto.fernandes775@gmail.com>
2020-03-30 17:03:57 -0300

    reconstrução da tela inicial

--- front/src/pages/PageHome.js
+++ front/src/pages/PageHome.js
@@ -1,41 +1,159 @@
 import * as React from 'react';
-import {View, StyleSheet} from 'react-native';
-import {Button} from '@ui-kitten/components';
+import {useNavigation} from '@react-navigation/native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+} from 'react-native';
 
-const HomeScreen = ({navigation}) => {
+import logoZumbi from '../../assets/logoZumbi1.png';
+import iconArma from '../../assets/iconArma.png';
+import iconArmadura from '../../assets/iconArmadura.jpg';
+import iconZumbi from '../../assets/zumbi.png';
+
+import imageBack from '../../assets/imageBack2.jpg';
+
+export default function HomeScreen() {
+  const navigation = useNavigation();
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Button
-        onPress={async () => {
-          await navigation.navigate('PageHomeArma');
-        }}>
-        Arma
-      </Button>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image style={styles.logoZumbi} source={logoZumbi} />
+        <Text style={styles.textHeader}>Crie seu exercito de Zumbis!</Text>
+      </View>
+      <View style={styles.actions}>
+        <TouchableOpacity
+          style={styles.action}
+          onPress={() => {
+            navigation.navigate('PageHomeArma');
+          }}>
+          <View style={styles.iconArma}>
+            <Image style={styles.iconImageArma} source={iconArma} />
+          </View>
+          <Text style={styles.actionText}>Arma</Text>
+        </TouchableOpacity>
 
-      <Button
-        onPress={async () => {
-          await navigation.navigate('PageHomeArmadura');
-        }}>
-        Armadura
-      </Button>
+        <TouchableOpacity
+          style={styles.action}
+          onPress={() => {
+            navigation.navigate('PageHomeZumbi');
+          }}>
+          <View style={styles.iconArmadura}>
+            <Image style={styles.iconImageArmadura} source={iconArmadura} />
+          </View>
+          <Text style={styles.actionText}>Armadura</Text>
+        </TouchableOpacity>
 
-      <Button
-        title="Zumbi"
-        onPress={async () => {
-          await navigation.navigate('PageHomeZumbi');
-        }}>
-        Zumbi
-      </Button>
+        <TouchableOpacity
+          style={styles.action}
+          onPress={() => {
+            navigation.navigate('PageHomeArma');
+          }}>
+          <View style={styles.iconArma}>
+            <Image style={styles.iconImageZumbi} source={iconZumbi} />
+          </View>
+          <Text style={styles.actionText}>Zumbi</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
-  separator: {
-    marginVertical: 8,
-    borderBottomColor: '#737373',
-    borderBottomWidth: StyleSheet.hairlineWidth,
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    padding: 20,
+    // margin: 10,
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  logoZumbi: {
+    aspectRatio: 1,
+    width: 80,
+    height: 30,
+    resizeMode: 'contain',
+    marginRight: 20,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30,
+  },
+  textHeader: {
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
+  iconImageArma: {
+    width: 30,
+    height: 30,
+  },
+  iconImageArmadura: {
+    width: 38,
+    height: 38,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+  },
+  iconImageZumbi: {
+    width: 35,
+    height: 35,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+  },
+  iconArma: {
+    backgroundColor: '#ffffff',
+    width: 38,
+    height: 38,
+    marginLeft: 6,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+  },
+  iconArmadura: {
+    width: 10,
+    height: 38,
+    marginLeft: 6,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+  },
+  actions: {
+    flex: 0.8,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    marginBottom: 80,
+  },
+  action: {
+    backgroundColor: '#000000',
+    borderRadius: 8,
+    height: 55,
+    width: 180,
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30,
+  },
+  actionText: {
+    color: '#FFF',
+    fontSize: 15,
+    fontWeight: 'bold',
+    alignItems: 'center',
+    paddingRight: 50,
   },
 });
-
-export default HomeScreen;
